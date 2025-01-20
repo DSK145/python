2cbec3aefaed7ac8dc754b26a436c7e2c5fba610
DSK145 <qazxcvbnmrdfg@qq.com>
2025-01-20 17:41:15 +0800

    更新

---
+++ python a.py
@@ -0,0 +1,17 @@
+print("字符串带那点点")
+a = 4
+s = "a"
+d = "100"
+prinr(a)
+print("a")   #区别
+print(s)
+
+print(bin(a))  #二进制bin
+print(oct(d))  #八进制oct
+print(hex(d))  #十六进制hex都要在()中
+
+num = 10       #定义变量
+print(bin(num)[2:])  #通过切片 [2:]去前缀，bin进制
+print(oct(num)[2:])  #和上方一样
+print(hex(num)[2:])  #和上方一样
+
